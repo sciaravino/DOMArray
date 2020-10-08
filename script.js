e7b5db678b7882f8c6a068577075e3b1e054f55f
sciaravino <59625798+sciaravino@users.noreply.github.com>
2020-10-07 21:23:22 -0400

    third commit

--- script.js
+++ script.js
@@ -29,4 +29,27 @@ async function getRandomUser(){
 // Add new obj to data arr
 function addData(obj){
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data){
+    // Clear the main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money
+function formatMoney(number){
+    return '$' + (number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
